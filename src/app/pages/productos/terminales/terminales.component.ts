3388da549425f1ab549cc9817b2a944464799593
Jonathansr16 <jonathansr2025@gmail.com>
2025-03-20 17:59:36 -0600

    styles: add new banner

--- src/app/pages/productos/terminales/terminales.component.ts
+++ src/app/pages/productos/terminales/terminales.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { CardProductComponent } from '@feature/components/card-product/card-product.component';
+import { ThemesService } from '@services/themes.service';
 
 @Component({
   selector: 'app-terminales',
-  imports: [],
-  template: `<p>terminales works!</p>`,
+  imports: [
+    // CardProductComponent
+  ],
+  templateUrl: 'terminales.component.html',
   styleUrl: './terminales.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TerminalesComponent { }
+export default class TerminalesComponent { 
+
+      theme = inject(ThemesService);
+      isDarkTheme = computed(() => this.theme.themeChange());
+  
+
+}

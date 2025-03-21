1fb21a785aa46b71c843f5e601b2f8c56045f8e7
Jonathansr16 <jonathansr2025@gmail.com>
2025-03-21 17:53:18 -0600

    add new section

--- src/app/pages/productos/terminales/terminales.component.ts
+++ src/app/pages/productos/terminales/terminales.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { CardProductComponent } from '@feature/components/card-product/card-product.component';
+import { DoubtsComponent } from '@feature/components/doubts/doubts.component';
+import { RegisterStepsComponent } from '@feature/components/register-steps/register-steps.component';
 import { ThemesService } from '@services/themes.service';
 
 @Component({
   selector: 'app-terminales',
   imports: [
-    // CardProductComponent
-  ],
+    CardProductComponent,
+    DoubtsComponent
+],
   templateUrl: 'terminales.component.html',
   styleUrl: './terminales.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
